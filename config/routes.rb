b44f49cabe67cad8a66c39dc337136d89229a284
layfieldk <kevinjlayfield@gmail.com>
2016-01-24 20:38:54 +0000

    Added log in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
+
 
   root 'users#new'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   
   resources :users
 end
